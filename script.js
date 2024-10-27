b172d87bb7413ad9853065dcbf4736ba310206b6
Henn-web <tanhenryarianto@gmail.com>
2024-10-27 11:42:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -71,3 +71,6 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img img, .services-container, .project-box, .testimonial-wrapper, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img img', { origin: 'left' });
 ScrollReveal().reveal('.home-content h3, .home-content p, .about-content', { origin: 'right' });
+
+
+
